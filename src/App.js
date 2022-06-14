996cdb6bafce70f3625f1a16f08c4681bd030f9c
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-06-14 11:41:12 -0500

    Add Profile page that will hold the reservations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import MyProfile from './pages/MyProfile';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/myprofile" element={<MyProfile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/profile/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Profile = () => (
+  <div>
+    Missions and Rockets coming soon!
+  </div>
+);
+
+export default Profile;
