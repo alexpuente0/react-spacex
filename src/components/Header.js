ce57e4fe16b4d16a5814c19a9ff31715f3b337af
Fernando <luisfernandosalasg@gmail.com>
2022-06-14 10:53:45 -0500

    Fix linters

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,21 @@
-import React from "react";
-import logo from "../assets/planet.png";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import logo from '../assets/planet.png';
 
-const Header = () => {
-	return (
-		<header>
-			<nav>
-				<div className="title-container">
-					<img src={logo} alt="logo" className="logo" />
-					<h1 className="header">Space Traveler&apos; Hub</h1>
-				</div>
-				<ul className="list-container">
-					<li>Rockets</li>
-					<li>Missions</li>
-					<li>My Profile</li>
-				</ul>
-			</nav>
-			<hr className="line" />
-		</header>
-	);
-};
+const Header = () => (
+  <header>
+    <nav>
+      <div className="title-container">
+        <img src={logo} alt="logo" className="logo" />
+        <h1 className="header">Space Traveler&apos; Hub</h1>
+      </div>
+      <ul className="list-container">
+        <li>Rockets</li>
+        <li>Missions</li>
+        <li>My Profile</li>
+      </ul>
+    </nav>
+    <hr className="line" />
+  </header>
+);
 
 export default Header;
