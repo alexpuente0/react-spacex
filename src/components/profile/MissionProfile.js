12f7df93630918cee437e65c313721c130965799
Fernando <luisfernandosalasg@gmail.com>
2022-06-15 10:28:15 -0500

    Add rockets profile

---
+++ src/components/profile/MissionProfile.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+import React from "react";
+
+export const MissionProfile = () => {
+	return <div>MissionProfile</div>;
+};
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,9 +1,25 @@
-import React from 'react';
+/* eslint-disable */
+import React from "react";
+import { useSelector } from "react-redux";
 
-const Profile = () => (
-  <div>
-    Missions and Rockets coming soon!
-  </div>
-);
+const Profile = () => {
+	const rocketState = useSelector((state) => state.rocketsReducer);
+	const reservedRockets = rocketState.filter((item) => item.reserved === true);
+
+	return (
+		<div className="profile-container">
+			<div className="rockets-profile">
+				<h2 className="title">My Rockets</h2>
+				<ul className="rockets">
+					{reservedRockets.map((item) => (
+						<li className="rockets-li" key={item.id}>
+							{item.name}
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+};
 
 export default Profile;
